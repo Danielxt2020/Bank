948e54af8554f3d1b4de6bb3e856e37a4e0b1735
danielxts555@gmail.com <danielxts555@gmail.com>
2021-03-29 20:36:19 -0300

    end

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using Bank.Entities;
+using Bank.Entities.Exceptions;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+                Console.WriteLine("Enter account data");
+                Console.Write("Number:");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance:");
+                double InitialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double WithdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+              
+
+                Account account = new Account(number, holder, InitialBalance, WithdrawLimit);
+
+                Console.Write("Enter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+
+                account.Withdraw(amount);
+                Console.WriteLine("New balancer:" + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine(" Erro " + e.Message);
+            }
+
+        }
+    }
+}
